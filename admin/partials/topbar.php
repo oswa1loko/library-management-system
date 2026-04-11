d8ca5e33f0119c930380a7b54ef732f43e106627
oswa1loko <joshuapasaporte@gmail.com>
2026-04-11 14:25:16 +0800

    Polish admin content surfaces and topbar

--- admin/partials/topbar.php
+++ admin/partials/topbar.php
@@ -3,8 +3,9 @@ $pageTitle = isset($pageTitle) ? (string) $pageTitle : 'Admin';
 $pageSubtitle = isset($pageSubtitle) ? (string) $pageSubtitle : '';
 $sidebarPage = isset($sidebarPage) ? (string) $sidebarPage : '';
 ?>
-<div class="topbar">
+<div class="topbar topbar-admin">
   <div>
+    <p class="topbar-kicker">Admin Workspace</p>
     <h1><?php echo h($pageTitle); ?></h1>
     <?php if ($pageSubtitle !== ''): ?>
       <p><?php echo h($pageSubtitle); ?></p>
