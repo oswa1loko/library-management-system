e390d2bed511cdb732d08fb228efedef7e0a46d2
oswa1loko <joshuapasaporte@gmail.com>
2026-04-12 10:54:14 +0800

    Rename admin topbar kicker to portal

--- admin/partials/topbar.php
+++ admin/partials/topbar.php
@@ -5,7 +5,7 @@ $sidebarPage = isset($sidebarPage) ? (string) $sidebarPage : '';
 ?>
 <div class="topbar topbar-admin">
   <div>
-    <p class="topbar-kicker">Admin Workspace</p>
+    <p class="topbar-kicker">Admin Portal</p>
     <h1><?php echo h($pageTitle); ?></h1>
     <?php if ($pageSubtitle !== ''): ?>
       <p><?php echo h($pageSubtitle); ?></p>
